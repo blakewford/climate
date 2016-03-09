58ea8bebb5e35b2635855d256c2580949e37ee1f
Blake W. Ford <blakewford@gmail.com>
2016-03-09 15:39:53 -0600

    Add more fields by user request

--- src/org/starlo/climate/ZipcodeInputFragment.java
+++ src/org/starlo/climate/ZipcodeInputFragment.java
@@ -7,14 +7,34 @@ import android.widget.*;
 
 public class ZipcodeInputFragment extends Fragment
 {
+    private ListView mList = null;
+    private ArrayAdapter mAdapter = null;
+    private final String[] DEFAULT_ZIP_CODES = new String[]{"78757", "78758", "78759"};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        final String[] zipcodes = new String[]{"78757", "78758", "78759"};
         View view = inflater.inflate(R.layout.zipcode_input, container, false);
-        ListView listView = (ListView)view.findViewById(R.id.list);
-        listView.setAdapter(new EditableArrayAdapter(getActivity(), 0, zipcodes));
+        mList = (ListView)view.findViewById(R.id.list);
+        mAdapter = new EditableArrayAdapter(getActivity(), 0, DEFAULT_ZIP_CODES);
+        mList.setAdapter(mAdapter);
+        Button addButton = (Button)view.findViewById(R.id.add);
+        addButton.setOnClickListener(new AddFieldListener());
 
         return view;
+    }
+
+    private class AddFieldListener implements View.OnClickListener
+    {
+        public void onClick(View view)
+        {
+            String[] newList = new String[mAdapter.getCount()+1];
+            for(int i = 0; i < DEFAULT_ZIP_CODES.length; i++)
+            {
+                newList[i] = DEFAULT_ZIP_CODES[i];
+            }
+            mAdapter = new EditableArrayAdapter(getActivity(), 0, newList);
+            mList.setAdapter(mAdapter);
+        }
     }
 }
